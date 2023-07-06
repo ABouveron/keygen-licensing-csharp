75cde2395aa21fbd129977bd833f8031bb4b8694
ABouveron <armand.bouveron@telecomnancy.eu>
2023-07-06 08:46:46 +0200

    style: csharpier

--- Test.cs
+++ Test.cs
@@ -19,8 +19,15 @@ public class Test
         Console.SetOut(stringWriter);
 
         // Run the application
-        Program.Main(new string[] { "examples/license.lic", "examples/machine.lic", "198e9fe586114844f6a4eaca5069b41a7ed43fb5a2df84892b69826d64573e39" });
-    
+        Program.Main(
+            new string[]
+            {
+                "examples/license.lic",
+                "examples/machine.lic",
+                "198e9fe586114844f6a4eaca5069b41a7ed43fb5a2df84892b69826d64573e39"
+            }
+        );
+
         // Assert the output is what we expected
         var output = stringWriter.ToString();
         _consoleOutput.WriteLine(output);
