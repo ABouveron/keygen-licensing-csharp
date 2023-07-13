8d886107fd103dcb477870ff7906cc13d1d74518
ABouveron <armand.bouveron@telecomnancy.eu>
2023-07-13 14:50:24 +0200

    feat: entitlement creation

--- KeygenApi.cs
+++ KeygenApi.cs
@@ -102,6 +102,28 @@ public abstract class KeygenApiTest
         return client.Execute(request);
     }
 
+    private static RestResponse EntitlementCreation(string name, string code)
+    {
+        var client = new RestClient("https://api.keygen.sh/v1/accounts/" + Environment.GetEnvironmentVariable("KEYGEN_ACCOUNT_ID"));
+        var request = new RestRequest("entitlements", Method.Post);
+ 
+        request.AddHeader("Content-Type", "application/vnd.api+json");
+        request.AddHeader("Accept", "application/vnd.api+json");
+        request.AddHeader("Authorization", "Bearer " + Environment.GetEnvironmentVariable("KEYGEN_ADMIN_TOKEN"));
+ 
+        request.AddJsonBody(new {
+            data = new {
+                type = "entitlements",
+                attributes = new {
+                    name,
+                    code
+                }
+            }
+        });
+ 
+        return client.Execute(request);
+    }
+
     public static void Main(string[] args)
     {
         DotNetEnv.Env.Load();
