0bdc0af35df099a08d0d353c0a7f658a9fc3aac9
ABouveron <armand.bouveron@telecomnancy.eu>
2023-07-17 11:28:33 +0200

    feat: better UX for API test

--- api/TestAPI.cs
+++ api/TestAPI.cs
@@ -5,9 +5,14 @@ namespace example_csharp_licensing_Docker.api;
 
 public abstract class TestApi
 {
-    public static void Main_aux(string[] args)
+    public static void Main_aux()
     {
         Console.WriteLine(Token.GenAdminToken().Content);
-        Console.WriteLine("\nGet the admin token from the response above and put it in .env. You can now edit api/TestAPI.cs.");
+        Console.WriteLine(
+            "\nGet the admin token from the response above and put it in .env. You can now edit api/TestAPI.cs."
+        );
+        Console.WriteLine(
+            "If there is no output and you are trying to contact localhost API, you probably did not execute the run script (you must execute install script before that)."
+        );
     }
 }
