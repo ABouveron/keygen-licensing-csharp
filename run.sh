b78c64c77b0cbb206ea0d5032c2acffa6c8edbe7
ABouveron <armand.bouveron@telecomnancy.eu>
2023-07-17 10:55:22 +0200

    fix: simplification of run script

--- run.sh
+++ run.sh
@@ -5,5 +5,4 @@ docker start /web.1
 docker start /worker.1
 
 # Reverse proxy
-echo '127.0.0.1 api.keygen.localhost' | sudo tee -a /etc/hosts
 caddy reverse-proxy --from api.keygen.localhost --to :3000
