d1ff3bf214db65aa7d0cd92709ec2fe0258f24f0
ABouveron <armand.bouveron@telecomnancy.eu>
2023-07-25 09:53:14 +0200

    feat: periodic license check

--- Program.cs
+++ Program.cs
@@ -4,14 +4,20 @@ public abstract class Program
 {
     public static async Task Main(string[]? args)
     {
+        Env.Load();
         if (args == null || args.Length == 0)
             MachineFile.Verification(args);
         else
             switch (args[0])
             {
+                case "license":
+                    await CheckInternet.CheckInternetLicense(args[1..]);
+                    return;
                 case "api":
-                    Env.Load();
-                    await CheckInternet.CheckInternetAsync(args[1..]);
+                    await CheckInternet.CheckInternetTest(args[1..]);
+                    return;
+                case "periodic":
+                    await CheckInternet.CheckInternetPeriodic(args[1..]);
                     return;
                 default:
                     MachineFile.Verification(args);
--- utilities/CheckInternet.cs
+++ utilities/CheckInternet.cs
@@ -4,7 +4,7 @@ public abstract class CheckInternet
 {
     public static string api = "api.keygen.sh";
 
-    public static async Task CheckInternetAsync(string[] args)
+    public static async Task CheckInternetTest(string[] args)
     {
         var myPing = new Ping();
         try
@@ -21,6 +21,43 @@ public abstract class CheckInternet
                 "Using official API did not succeed...\n" + "Now trying to use machine file..."
             );
             MachineFile.Verification(args);
+        }
+    }
+
+    public static async Task CheckInternetLicense(string[] args)
+    {
+        var myPing = new Ping();
+        try
+        {
+            Console.WriteLine("Début Ping");
+            await myPing.SendPingAsync("google.com", 3000, new byte[32], new PingOptions(64, true));
+            Console.WriteLine("Fin Ping");
+
+            // use official API api.keygen.sh
+            Console.WriteLine("Using official API...");
+            api = "api.keygen.sh";
+            example_csharp_licensing_Docker.api.Program.LicenseActivationMain(args);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine(
+                "Using official API did not succeed...\n" + "Now trying to use machine file..."
+            );
+            MachineFile.Verification(args[2..]);
+        }
+    }
+
+    public static async Task CheckInternetPeriodic(string[] args)
+    {
+        await PeriodicCheck(args, TimeSpan.FromSeconds(10));
+    }
+
+    private static async Task PeriodicCheck(string[] args, TimeSpan interval, CancellationToken cancellationToken = default)
+    {
+        while (true)
+        {
+            await CheckInternetLicense(args);
+            await Task.Delay(interval, cancellationToken);
         }
     }
 }
